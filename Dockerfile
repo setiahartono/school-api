3080a0eed121305bc5f20f5669af6210f0fe56a9
Bayu Sasrabau Setiahartono <bayu.setiahartono@happyfresh.com>
2022-10-17 20:58:43 +0700

    Dockerization

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM python:3.7
+FROM python:3.8
 ENV PYTHONUNBUFFERED 1
-RUN mkdir /code
 WORKDIR /code
-ADD requirements.txt /code/
-ADD . /code/
+RUN pip install pipenv
+
+ADD requirements.txt ./
+
 RUN pip install -r requirements.txt
-RUN python manage.py makemigrations
-ENV DJ_SETTINGS core.settings_docker
+ADD . ./
--- core/settings_docker.py
+++ core/settings_docker.py
@@ -2,4 +2,19 @@ from core.settings import *
 
 ALLOWED_HOSTS = ["0.0.0.0"]
 
+USE_CACHE = True
+
+SECRET_KEY = 'django-insecure-&s$0xki157_)(sv-d61sp(#6y*7g%9nord4f2mi$nuhux2x&m4'
+
 CACHES['default']['LOCATION'] = "redis://redis-cache:6379/"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
+    }
+}
